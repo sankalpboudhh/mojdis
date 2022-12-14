1327154b9cda8723024ea1ab7bfc4e876ed291e4
Sankalp Boudhh <sankalpboudhh@gmail.com>
2022-12-14 22:18:41 +0530

    Modal Color

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: "background.paper",
+  bgcolor: "#cfc4b2",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
